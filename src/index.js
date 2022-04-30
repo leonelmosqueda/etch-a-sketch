413524f54d17e4e3129032bc069bc00d18247915
maury <maurymosqueda@gmail.com>
2022-04-30 18:52:53 -0300

    Create default grid

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const DEFAULT_SIZE = 16;
+const DEFAULT_COLOR = '#333333';
+const DEFAULT_MODE = 'color-mode';
+
+const $gridContainer = document.querySelector('#grid-container');
+
+window.addEventListener('load', () => {
+    createGrid(DEFAULT_SIZE);
+})
+
+function createGrid (size) {
+    $gridContainer.setAttribute('style', `display: grid;
+    grid-template-columns: repeat(${size}, 1fr);
+    grid-template-rows: repeat(${size}, 1fr);
+    `);
+
+    for (let i = 1; i <= size*size; i++) {
+        const newGrid = document.createElement('div');
+        newGrid.classList.add('grid-item');
+
+        $gridContainer.appendChild(newGrid);
+    }
+}
